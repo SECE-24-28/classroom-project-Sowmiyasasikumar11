1efcc9d4b994aa0138d82e3cb0e851dc88503e2b
Sowmiyasasikumar11 <sowmiya110307@gmail.com>
2025-12-12 22:05:51 +0530

    Add Day-9 folder with Axios HTTP requests and feedback application

---
+++ Day-9/feedback/src/Home.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react'
+import DataContext from './Context/DataContext'
+import Search from './Search'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+      const {searchResult}=useContext(DataContext)
+  return (
+    <div>
+        <Search />  
+        {
+        searchResult.map((post)=>
+           <div key={post.id} className="post">
+        <Link to={`/editpost/${post.id}`}>
+              <h3>{post.title}</h3>
+              <p>{post.datetime}</p>
+              <p>{post.body}</p>
+        </Link>
+        <hr />
+        </div>
+     )
+     }
+    </div>
+  )
+}
+
+export default Home
